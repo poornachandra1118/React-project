15a0c39fad6d57c8d5d85da162925a521a3921d6
poornachandra1118 <poornachandrak2002@gmail.com>
2024-12-29 20:31:11 +0530

    Update Navbar.js
    OP

--- src/Navbar.js
+++ src/Navbar.js
@@ -23,10 +23,7 @@ const Navbar_1 = (prop) => {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-              // {/*<Link to="/Home">Home</Link>*/}
-            //  <Nav.Link href="/Home" >
-            //   Home
-            // </Nav.Link>
+              
             
             <Nav.Link href="#action2">Service</Nav.Link>
             {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
